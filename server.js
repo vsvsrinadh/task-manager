8fbbaf8d454f822906245ade8ee9d23767bbde05
vsvsrinadh <srinadhveeravalli123@gmail.com>
2025-02-04 23:55:44 +0530

    Fixed module import issue in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const taskRoutes = require('./routes/taskroutes'); 
+const taskRoutes = require('./routes/taskRoutes'); 
 
 
 const app = express();
